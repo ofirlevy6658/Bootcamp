650c13dcb13639754ca67721c1e2035e3bf206c0
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-03-03 13:40:24 +0200

    movie-api

---
+++ bootcamp-exercises/js/api/movie-api/script.js
@@ -0,0 +1,51 @@
+const url = "http://www.omdbapi.com/?apikey=86ae5f35&t=";
+const btn = document.querySelector(".btn");
+let userSearch = document.querySelector("#movie");
+const wrongMovie = document.querySelector("#notFound");
+btn.addEventListener("click", displayData);
+
+//
+const request = async (movie) => {
+	const resposne = await fetch(
+		`http://www.omdbapi.com/?apikey=86ae5f35&t=${movie}`
+	);
+
+	return resposne.json();
+};
+async function displayData() {
+	const data = await request(userSearch.value);
+	const movie = {
+		poster: data.Poster,
+		title: data.Title,
+		genere: data.Genre,
+		year: data.Year,
+		plot: data.Plot,
+		director: data.Director,
+		actors: data.Actors,
+		ratings: data.Ratings,
+	};
+
+	// if (data.re === 404) {
+	// 	worngUser.style.visibility = "visible";
+	// 	return;
+	// }
+	// 	worngUser.style.visibility = "hidden";
+	const container = document.querySelector(".container");
+
+	const dataElement = `<div>
+	 							<img src="${movie.poster}" width="200px">
+	 							<h1>${movie.title}</h1>
+	 							<h2>${movie.genere}</h2>
+                                <h4>${movie.year}<h4>
+                                <p>${movie.plot}</p>
+                                <h4>Director</h4>
+                                <p>${movie.director}</p>
+                                <h4>Actors</h4>
+                                <p>${movie.actors}</p>
+                                <h4>Rating</h4>
+	 						</div>`;
+	const divMovie = document.querySelector(".movie");
+	divMovie.innerHTML = dataElement;
+	divMovie.style.border = "solid black 3px";
+	container.appendChild(divMovie);
+}
