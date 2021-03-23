502d98dff7867cf04110829a32ebad4f3ff6718a
ofirlevy6658 <ofirlevy6658@gmail.com>
2021-03-23 20:08:36 +0200

    update

--- bootcamp-exercises/React/router/src/components/App.js
+++ bootcamp-exercises/React/router/src/components/App.js
@@ -1,10 +1,11 @@
 import { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./Home";
 import Products from "./Products";
 import Header from "./Header";
 import ProductDetail from "./ProductDetail";
+import NotFound from "./NotFound";
 
 class App extends Component {
 	render() {
@@ -13,9 +14,12 @@ class App extends Component {
 				<BrowserRouter>
 					<div>
 						<Header />
-						<Route path="/" exact component={Home} />
-						<Route path="/products" exact component={Products} />
-						<Route path="/products/:id" exact component={ProductDetail} />
+						<Switch>
+							<Route path="/" exact component={Home} />
+							<Route path="/products" exact component={Products} />
+							<Route path="/products/:id" exact component={ProductDetail} />
+							<Route path="/" component={NotFound} />
+						</Switch>
 					</div>
 				</BrowserRouter>
 			</div>
---
+++ bootcamp-exercises/React/router/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+	return (
+		<>
+			<h1>404 page not found</h1>
+		</>
+	);
+};
+
+export default NotFound;
